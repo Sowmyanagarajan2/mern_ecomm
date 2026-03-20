4c8fc78caf1debe808b46a934eaab0428431735c
Sowmyanagarajan2 <sowmyanagarajan2.com>
2026-03-20 14:44:41 +0530

    add backend

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,11 @@ app.use("/api/products", productRoutes)
 app.use("/api/orders", orderRoutes)
 app.use("/api/auth", authRoutes)
 
-app.listen(5000,()=>{
- console.log("Server running on 5000")
-})
+// app.listen(5000,()=>{
+//  console.log("Server running on 5000")
+// })
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
