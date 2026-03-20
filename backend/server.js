16ecae08f879d0d34587db4cf352de4ee80abe93
Sowmyanagarajan2 <sowmyanagarajan2.com>
2026-03-20 17:16:59 +0530

    add root route

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,9 @@ app.use("/api/auth", authRoutes)
 // app.listen(5000,()=>{
 //  console.log("Server running on 5000")
 // })
+app.get("/", (req, res) => {
+  res.send("Backend is working 🚀");
+});
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
